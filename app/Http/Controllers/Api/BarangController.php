af40f0f148b1f197d03c25a731dd743a5827ff83
Riska Kurnia Triwulandari <riskakurniatriwulandari@gmail.com>
2024-05-07 20:21:52 +0700

    Tugas JS 11

--- app/Http/Controllers/Api/BarangController.php
+++ app/Http/Controllers/Api/BarangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\BarangModel;
+use Illuminate\Support\Facades\Validator;
 
 class BarangController extends Controller
 {
@@ -15,8 +16,30 @@ class BarangController extends Controller
 
     public function store(Request $request)
     {
+        //set validator
+        $validator = Validator::make($request->all(), [
+            'barang_kode' => 'required',
+            'barang_nama' => 'required',
+            'harga_beli' => 'required',
+            'harga_jual' => 'required',
+            'kategori_id' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+        //if validations fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
         $barang = BarangModel::create($request->all());
-        return response()->json($barang, 201);
+        if ($barang) {
+            return response()->json([
+                'success' => true,
+                'barang' => $barang,
+            ], 201);
+        }
+        //return JSON process insert failed
+        return response()->json([
+            'success' => false,
+        ], 409);
     }
 
     public function show(BarangModel $barang)
@@ -26,8 +49,32 @@ class BarangController extends Controller
 
     public function update(Request $request, BarangModel $barang)
     {
-        $barang->update($request->all());
-        return BarangModel::find($barang);
+        $validator = Validator::make($request->all(), [
+            'barang_kode' => 'required',
+            'barang_nama' => 'required',
+            'harga_beli' => 'required',
+            'harga_jual' => 'required',
+            'kategori_id' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+        //if validations fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        BarangModel::find($barang)->update($request->all(), [
+            $request->image->hashName()
+        ]);
+        $barang = BarangModel::with('kategori')->find($barang);
+        if ($barang) {
+            return response()->json([
+                'success' => true,
+                'barang' => $barang,
+            ], 201);
+        }
+        //return JSON process insert failed
+        return response()->json([
+            'success' => false,
+        ], 409);
     }
 
     public function destroy(BarangModel $barang)
