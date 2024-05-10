7535ee4295e412b6676167b347818361807f28a3
Riska Kurnia Triwulandari <riskakurniatriwulandari@gmail.com>
2024-05-10 11:58:23 +0700

    Validasi File Upload

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -14,20 +14,22 @@ class FileUploadController extends Controller
     {
         // dump($request->berkas);
         // return "Pemrosesan file upload di sini";
-        if ($request->hasFile('berkas')) {
-            echo "path(): " . $request->berkas->path();
-            echo "<br>";
-            echo "extension(): " . $request->berkas->extension();
-            echo "<br>";
-            echo "getClientOriginalExtension(): " . $request->berkas->getClientOriginalExtension();
-            echo "<br>";
-            echo "getMimeType(): " . $request->berkas->getMimeType();
-            echo "<br>";
-            echo "getClientOriginalName(): " . $request->berkas->getClientOriginalName();
-            echo "<br>";
-            echo "getSize(): " . $request->berkas->getSize();
-        } else {
-            echo "Tidak ada berkas yang diupload";
-        }
+        // if ($request->hasFile('berkas')) {
+        //     echo "path(): " . $request->berkas->path();
+        //     echo "<br>";
+        //     echo "extension(): " . $request->berkas->extension();
+        //     echo "<br>";
+        //     echo "getClientOriginalExtension(): " . $request->berkas->getClientOriginalExtension();
+        //     echo "<br>";
+        //     echo "getMimeType(): " . $request->berkas->getMimeType();
+        //     echo "<br>";
+        //     echo "getClientOriginalName(): " . $request->berkas->getClientOriginalName();
+        //     echo "<br>";
+        //     echo "getSize(): " . $request->berkas->getSize();
+        // } else {
+        //     echo "Tidak ada berkas yang diupload";
+        // }
+        $request->validate(['berkas' => 'required|file|image|max:500']);
+        echo $request->berkas->getClientOriginalName() . "lolos validasi";
     }
 }
