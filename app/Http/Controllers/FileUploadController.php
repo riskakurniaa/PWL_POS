7b9f972e87c582aa1fc1e2d4e21f497827f5dc7a
Riska Kurnia Triwulandari <riskakurniatriwulandari@gmail.com>
2024-05-10 12:24:00 +0700

    Memindah File Upload

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -12,24 +12,10 @@ class FileUploadController extends Controller
     }
     public function prosesFileUpload(Request $request)
     {
-        // dump($request->berkas);
-        // return "Pemrosesan file upload di sini";
-        // if ($request->hasFile('berkas')) {
-        //     echo "path(): " . $request->berkas->path();
-        //     echo "<br>";
-        //     echo "extension(): " . $request->berkas->extension();
-        //     echo "<br>";
-        //     echo "getClientOriginalExtension(): " . $request->berkas->getClientOriginalExtension();
-        //     echo "<br>";
-        //     echo "getMimeType(): " . $request->berkas->getMimeType();
-        //     echo "<br>";
-        //     echo "getClientOriginalName(): " . $request->berkas->getClientOriginalName();
-        //     echo "<br>";
-        //     echo "getSize(): " . $request->berkas->getSize();
-        // } else {
-        //     echo "Tidak ada berkas yang diupload";
-        // }
         $request->validate(['berkas' => 'required|file|image|max:500']);
-        echo $request->berkas->getClientOriginalName() . "lolos validasi";
+        $extFile = $request->berkas->getClientOriginalName();
+        $namaFile = 'web-' . time() . "." . $extFile;
+        $path = $request->berkas->storeAs('uploads', $namaFile);
+        echo "proses upload berhasil, file berada di: $path";
     }
 }
